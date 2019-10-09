9e4f50d93c73e72b6a6d93737def5b44116dfe92
xsa <xsa24@icloud.com>
2019-10-09 18:59:07 +0300

    init repo

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+
+namespace MulticlassClassification {
+    class Program {
+        private static string _appPath => Path.GetDirectoryName (Environment.GetCommandLineArgs () [0]);
+        private static string _trainDataPath => Path.Combine (_appPath, "..", "..", "..", "Data", "issues_train.tsv");
+        private static string _testDataPath => Path.Combine (_appPath, "..", "..", "..", "Data", "issues_test.tsv");
+        private static string _modelPath => Path.Combine (_appPath, "..", "..", "..", "Models", "model.zip");
+        private static MLContext _mlContext;
+        private static PredictionEngine<GitHubIssue, IssuePrediction> _predEngine;
+        private static ITransformer _trainedModel;
+        static IDataView _trainingDataView;
+
+        public static void Main (string[] args) {
+            _mlContext = new MLContext (seed: 0);
+            _trainingDataView = _mlContext.Data.LoadFromTextFile<GitHubIssue> (_trainDataPath, hasHeader : true);
+            var pipeline = ProcessData ();
+            var trainingPipeline = BuildAndTrainModel (_trainingDataView, pipeline);
+            Evaluate (_trainingDataView.Schema);
+            PredictIssue ();
+        }
+
+        public static IEstimator<ITransformer> ProcessData () {
+            var pipeline = _mlContext.Transforms.Conversion.MapValueToKey (inputColumnName: "Area", outputColumnName: "Label")
+                .Append (_mlContext.Transforms.Text.FeaturizeText (inputColumnName: "Title", outputColumnName: "TitleFeaturized"))
+                .Append (_mlContext.Transforms.Text.FeaturizeText (inputColumnName: "Description", outputColumnName: "DescriptionFeaturized"))
+                .Append (_mlContext.Transforms.Concatenate ("Features", "TitleFeaturized", "DescriptionFeaturized"))
+                .AppendCacheCheckpoint (_mlContext);
+            return pipeline;
+        }
+
+        public static IEstimator<ITransformer> BuildAndTrainModel (IDataView trainingDataView, IEstimator<ITransformer> pipeline) {
+            var trainingPipeline = pipeline.Append (_mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy ("Label", "Features"))
+                .Append (_mlContext.Transforms.Conversion.MapKeyToValue ("PredictedLabel"));
+
+            _trainedModel = trainingPipeline.Fit (trainingDataView);
+            _predEngine = _mlContext.Model.CreatePredictionEngine<GitHubIssue, IssuePrediction> (_trainedModel);
+
+            /*
+            Test Here
+             */
+            GitHubIssue issue = new GitHubIssue () {
+                Title = "WebSockets communication is slow in my machine",
+                Description = "The WebSockets communication used under the covers by SignalR looks like is going slow in my development machine.."
+            };
+
+            var prediction = _predEngine.Predict (issue);
+
+            return trainingPipeline;
+        }
+
+        public static void Evaluate (DataViewSchema trainingDataViewSchema) {
+            var testDataView = _mlContext.Data.LoadFromTextFile<GitHubIssue> (_testDataPath, hasHeader : true);
+            var testMetrics = _mlContext.MulticlassClassification.Evaluate (_trainedModel.Transform (testDataView));
+
+            Console.WriteLine ($"*************************************************************************************************************");
+            Console.WriteLine ($"*       Metrics for Multi-class Classification model - Test Data     ");
+            Console.WriteLine ($"*------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine ($"*       MicroAccuracy:    {testMetrics.MicroAccuracy:0.###}");
+            Console.WriteLine ($"*       MacroAccuracy:    {testMetrics.MacroAccuracy:0.###}");
+            Console.WriteLine ($"*       LogLoss:          {testMetrics.LogLoss:#.###}");
+            Console.WriteLine ($"*       LogLossReduction: {testMetrics.LogLossReduction:#.###}");
+            Console.WriteLine ($"*************************************************************************************************************");
+
+            SaveModelAsFile (_mlContext, trainingDataViewSchema, _trainedModel);
+        }
+
+        private static void SaveModelAsFile (MLContext mLContext, DataViewSchema trainingDataViewSchema, ITransformer model) {
+            mLContext.Model.Save (model, trainingDataViewSchema, _modelPath);
+        }
+
+        private static void PredictIssue () {
+            ITransformer loadedModel = _mlContext.Model.Load (_modelPath, out var modelInputSchema);
+            GitHubIssue singleIssue = new GitHubIssue () { Title = "Threads are failed", Description = "When i am use Threads my variables null take strong and not fail to crash." };
+            _predEngine = _mlContext.Model.CreatePredictionEngine<GitHubIssue, IssuePrediction> (loadedModel);
+            var prediction = _predEngine.Predict (singleIssue);
+            Console.WriteLine ($"=============== Single Prediction - Result: {prediction.Area} ===============");
+
+            GitHubIssue secondIssue = new GitHubIssue () { Title = "Entity Framework crashes", Description = "When connecting to the database, EF is crashing" };
+            _predEngine = _mlContext.Model.CreatePredictionEngine<GitHubIssue, IssuePrediction> (loadedModel);
+            var prediction2 = _predEngine.Predict (secondIssue);
+            Console.WriteLine ($"=============== Second Prediction - Result: {prediction2.Area} ===============");
+
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Multiclass Classification
+
+## Questions
+
+[Documentation](https://docs.microsoft.com/ru-ru/dotnet/machine-learning/resources/tasks#multiclass-classification)
+[Tutorial](https://docs.microsoft.com/ru-ru/dotnet/machine-learning/tutorials/github-issue-classification)
+
+## Most actual
+
+- Модель постоянно обучается, это нужно поправить например добавив входящие параметры для обучения и для выполнения
+- Для использования через другую программу можно использовать это в REST сервисах
